6996b12862afbeeff69403e851e31201ddb1a8d4
James Schneider <jschneider98@gmail.com>
2019-05-28 10:34:37 -0700

    new test

---
+++ util/query_int_builder_test.go
@@ -0,0 +1,30 @@
+// +build integration
+
+package util
+
+import (
+	"testing"
+)
+
+//
+func TestNewQueryIntBuilder(t *testing.T) {
+	InitTestCtx()
+	rawQuery := "test"
+	builder := NewQueryIntBuilder(testCtx, rawQuery)
+
+	if rawQuery != builder.RawQuery {
+		t.Errorf("Constructor fail. Expected '%s' Got: '%s'", rawQuery, builder.RawQuery)
+	}
+}
+
+//
+func TestBuild(t *testing.T) {
+	rawQuery := "super (barbosa or schneider) and not news"
+	builder := NewQueryIntBuilder(testCtx, rawQuery)
+
+	_, err := builder.Build()
+
+	if err != nil {
+		t.Error(err)
+	}
+}
