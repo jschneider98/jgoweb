9df93f0a630dc67cba7e5750ef08a84cee5c5794
James Schneider <jschneider98@gmail.com>
2019-05-27 18:18:06 -0700

    New context interface

---
+++ context_interface.go
@@ -0,0 +1,15 @@
+package jgoweb
+
+import(
+	"github.com/gocraft/dbr"
+)
+
+type ContextInterface interface {
+	Begin() (*dbr.Tx, error)
+	Commit() (error)
+	Rollback() (error)
+	Select(column ...string) *dbr.SelectBuilder
+	SelectBySql(query string, value ...interface{}) *dbr.SelectBuilder
+	OptionalBegin() (*dbr.Tx, error)
+	OptionalCommit(tx *dbr.Tx) error
+}
