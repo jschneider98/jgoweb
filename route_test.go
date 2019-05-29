711c63e442b81b24cd21d025d7137eec47d2c612
James Schneider <jschneider98@gmail.com>
2019-05-28 23:03:02 -0700

    More tests

--- route_test.go
+++ route_test.go
@@ -17,12 +17,25 @@ func (ctx *WebContext) SetTestUser(rw web.ResponseWriter, req *web.Request, next
 }
 
 // GET: Index route
-func (ctx *WebContext) index(rw web.ResponseWriter, req *web.Request) {
+func (ctx *WebContext) testRouteIndex(rw web.ResponseWriter, req *web.Request) {
 	ctx.JobSuccess()
 }
 
-// GET: Index route
-func (ctx *WebContext) routeError(rw web.ResponseWriter, req *web.Request) {
+
+// GET: json ok route
+func (ctx *WebContext) testRouteJsonOk(rw web.ResponseWriter, req *web.Request) {
+	ctx.JsonOkResponse(rw, 200, "ok")
+}
+
+// GET: json error route
+func (ctx *WebContext) testRouteJsonError(rw web.ResponseWriter, req *web.Request) {
+	err := errors.New("Forced error")
+	ctx.JobWarning("test", err)
+	ctx.JsonErrorResponse(rw, 500, err)
+}
+
+// GET: error route
+func (ctx *WebContext) testRouteError(rw web.ResponseWriter, req *web.Request) {
 	err := errors.New("Forced error")
 	ctx.JobError(util.WhereAmI(), err)
 }
@@ -37,23 +50,25 @@ func TestRoutes(t *testing.T) {
 		Middleware((*WebContext).LoadTemplate).
 		Middleware((*WebContext).LoadJob).
 		Middleware((*WebContext).LoadSession).
-		Get("/index", (*WebContext).index)
+		Get("/index", (*WebContext).testRouteIndex)
 
 	router.Subrouter(WebContext{}, "/").
 		Middleware((*WebContext).RequireUser).
-		Get("/require_user", (*WebContext).index)
+		Get("/require_user", (*WebContext).testRouteIndex)
 
 	router.Subrouter(WebContext{}, "/").
 		Middleware((*WebContext).AjaxRequireUser).
-		Get("/ajax_require_user", (*WebContext).index)
+		Get("/ajax_require_user", (*WebContext).testRouteIndex)
 
 	router.Subrouter(WebContext{}, "/").
 		Middleware((*WebContext).SetTestUser).
 		Middleware((*WebContext).RequireUser).
-		Get("/logged_in_index", (*WebContext).index)
+		Get("/logged_in_index", (*WebContext).testRouteIndex)
 
 	router.Subrouter(WebContext{}, "/").
-		Get("/forced_error", (*WebContext).routeError)
+		Get("/json_ok", (*WebContext).testRouteJsonOk).
+		Get("/json_error", (*WebContext).testRouteJsonError).
+		Get("/forced_error", (*WebContext).testRouteError)
 
 	rw, req := NewTestRequest("GET", "/index", nil)
 	router.ServeHTTP(rw, req)
@@ -73,6 +88,16 @@ func TestRoutes(t *testing.T) {
 	rw, req = NewTestRequest("GET", "/logged_in_index", nil)
 	router.ServeHTTP(rw, req)
 	AssertResponse(t, rw, 200)
+
+	//
+	rw, req = NewTestRequest("GET", "/json_ok", nil)
+	router.ServeHTTP(rw, req)
+	AssertResponse(t, rw, 200)
+
+	//
+	rw, req = NewTestRequest("GET", "/json_error", nil)
+	router.ServeHTTP(rw, req)
+	AssertResponse(t, rw, 500)
 
 	//
 	rw, req = NewTestRequest("GET", "/forced_error", nil)
