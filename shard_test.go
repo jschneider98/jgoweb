b186fa59750c44f3c296b6a5bb6639f9487cd75f
James Schneider <jschneider98@gmail.com>
2019-05-28 20:50:35 -0700

    New shard test

---
+++ shard_test.go
@@ -0,0 +1,45 @@
+// +build integration
+
+package jgoweb
+
+import (
+	"testing"
+)
+
+//
+func TestFetchShardByAccountId(t *testing.T) {
+	InitMockCtx()
+
+	accountId := "bad_id"
+
+	shard, err := FetchShardByAccountId(MockCtx, accountId)
+
+	if err == nil {
+		t.Errorf("\nERROR: non-uuid should return an error\n")
+	}
+
+	accountId = "00000000-0000-0000-0000-000000000000"
+
+	shard, err = FetchShardByAccountId(MockCtx, accountId)
+
+	if err != nil {
+		t.Errorf("\nERROR: %v\n", err)
+	}
+
+	if shard != nil {
+		t.Errorf("\nInvalid account ID should return nil.\n")
+	}
+
+	// @TEMP, @TODO: Build a MockAccount. Hard coding accountId for now.
+	accountId = "d1d44049-d0b3-4cf3-90ec-b980bf9d1705"
+
+	shard, err = FetchShardByAccountId(MockCtx, accountId)
+
+	if err != nil {
+		t.Errorf("\nERROR: %v\n", err)
+	}
+
+	if shard == nil {
+		t.Errorf("\nValid account ID should return a shard.\n")
+	}
+}
