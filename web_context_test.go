ec5dfb32699f7e236d61429dea3ce53bbc56cf35
James Schneider <jschneider98@gmail.com>
2019-05-28 22:43:27 -0700

    web context test

---
+++ web_context_test.go
@@ -0,0 +1,36 @@
+// +build integration
+
+package jgoweb
+
+import (
+	"testing"
+)
+
+//
+func TestTransactions(t *testing.T) {
+	InitMockCtx()
+
+	_, err := MockCtx.Begin()
+
+	if err != nil {
+		t.Errorf("\nERROR: Failed to start transaction %v\n", err)
+	}
+
+	err = MockCtx.Rollback()
+
+	if err != nil {
+		t.Errorf("\nERROR: Failed to rollback transaction %v\n", err)
+	}
+
+	_, err = MockCtx.Begin()
+
+	if err != nil {
+		t.Errorf("\nERROR: Failed to start transaction %v\n", err)
+	}
+
+	err = MockCtx.Commit()
+
+	if err != nil {
+		t.Errorf("\nERROR: Failed to commit transaction %v\n", err)
+	}
+}
