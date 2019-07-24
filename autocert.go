89065108bbef61dbfafcf1bdfa87fd3754a5e5b2
James Schneider <jschneider98@gmail.com>
2019-07-24 14:42:45 -0700

    New auto cert cache factory

---
+++ autocert.go
@@ -0,0 +1,28 @@
+package jgoweb
+
+import(
+	"io/ioutil"
+	"encoding/json"
+	"golang.org/x/crypto/acme/autocert"
+	"github.com/jschneider98/jgocache/autocert/cache"
+)
+
+//
+var NewAutocertCache = func() (autocert.Cache, error) {
+	options := make(map[string]string)
+
+	file, err := ioutil.ReadFile("./autocert.json")
+	
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(file, &options)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// @TODO: Change interface to just accept options
+	return cache.NewCacheFactory("sql", options)
+}
