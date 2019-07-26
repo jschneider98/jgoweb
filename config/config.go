5aeada84ef278d0f55386ed604ddf64f9305ce0b
James Schneider <jschneider98@gmail.com>
2019-07-25 19:55:56 -0700

    New config

---
+++ config/config.go
@@ -0,0 +1,103 @@
+package config
+
+import (
+	"io/ioutil"
+	"encoding/json"
+	"golang.org/x/crypto/acme/autocert"
+	// "github.com/jschneider98/jgocache/autocert/cache"
+)
+
+// Config file definition
+type Config struct {
+	ServerOptions ServerOptions `json:"server"`
+	DbConnOptions []DbConnOptions `json:"dbConns"`
+	AutocertCacheOptions AutocertCacheOptions `json:"autocertCache"`
+	AutocertCache autocert.Cache `json:"-"`
+	AutocertOptions AutocertOptions `json:"autocert"`
+}
+
+// Server configuratoin
+type ServerOptions struct {
+	SessionName string `json:sessionName`
+	SessionKey string `json:sessionKey`
+	EnableSsl bool `json:enableSsl`
+	HttpsHost string `json:httpsHost`
+	HttpHost string `json:httpHost`
+	HealthHost string `json:healthHost`
+}
+
+// DB Connection Strings
+type DbConnOptions struct {
+	ShardName string `json:"shardName"`
+	Dsn string `json:"dsn"`
+}
+
+// AutocertCache configuration
+type AutocertCacheOptions struct {
+	BackendOptions map[string]string `json:"backendOptions"`
+}
+
+// Autocert configuration
+type AutocertOptions struct {
+	AllowedHost string `json:"allowedHost"`
+	Email string `json:"email"`
+	DirectoryURL string `json:"directoryURL"`
+}
+
+// Reads json configuration file and returns Config
+func New(path string) (*Config, error) {
+	file, err := read(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	config, err := parse(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	config.EnsureBasicOptions()
+
+	return config, nil
+}
+
+// Conditionally load default values 
+func (c *Config) EnsureBasicOptions() {
+
+	if c.ServerOptions.SessionName == "" {
+		c.ServerOptions.SessionName = "web-session"
+	}
+
+	if c.ServerOptions.SessionKey == "" {
+		c.ServerOptions.SessionKey = "u46IpCV9y5Vjsi5YvODJEhgOY8m9JVE4"
+	}
+
+	// Default acme URL
+	if c.AutocertOptions.DirectoryURL == "" {
+		c.AutocertOptions.DirectoryURL = "https://acme-v01.api.letsencrypt.org/directory"
+	}
+}
+
+func read(path string) ([]byte, error) {
+	file, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func parse(jsonConfig []byte) (*Config, error) {
+	var config Config
+
+	err := json.Unmarshal(jsonConfig, &config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
