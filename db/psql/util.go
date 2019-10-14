26075e3dce065d924d15b80873b3f3d85f50a3a4
James Schneider <jschneider98@gmail.com>
2019-10-14 11:37:58 -0700

    Added ToCamelCase() etc

--- db/psql/util.go
+++ db/psql/util.go
@@ -1,31 +1,40 @@
 package psql
 
 import (
+	"fmt"
+	"regexp"
+	"strings"
 	"github.com/gocraft/dbr"
 	"github.com/jschneider98/jgoweb"
+	"github.com/jschneider98/jgoweb/util"
 )
 
 type Field struct {
+	DbFieldName string `json:"db_field_name"`
+	DbDataType string `json:"db_data_type"`
+	DbDefault dbr.NullString `json:"db_default"`
+	DbDefaultIsFunc bool `json:"db_default_is_func`
 	FieldName string `json:"field_name"`
 	DataType string `json:"data_type"`
-	Default dbr.NullString `json:"default"`
+	Default string `json:"default"`
+	Annotation string `json:"annotation"`
 	NotNull bool `json:"not_null"`
-	Attnum int `json:"attnum"`
+	SortNum int `json:"sort_num"`
 }
 
 func GetFields(ctx jgoweb.ContextInterface, schema string, table string) ([]Field, error) {
 	var fields []Field
 
 	stmt := ctx.Select(`
-		a.attname as field_name,
-		pg_catalog.format_type(a.atttypid, a.atttypmod) as data_type,
+		a.attname as db_field_name,
+		pg_catalog.format_type(a.atttypid, a.atttypmod) as db_data_type,
 		(
 			SELECT substring(pg_catalog.pg_get_expr(d.adbin, d.adrelid) for 128)
 			FROM pg_catalog.pg_attrdef d
 			WHERE d.adrelid = a.attrelid AND d.adnum = a.attnum AND a.atthasdef
-		) as default,
+		) as db_default,
 		a.attnotnull as not_null,
-		a.attnum
+		a.attnum as sort_num
 	`).
 	From(dbr.I("pg_catalog.pg_class").As("c")).
 	Join(dbr.I("pg_catalog.pg_namespace").As("n"), "n.oid = c.relnamespace").
@@ -42,5 +51,109 @@ func GetFields(ctx jgoweb.ContextInterface, schema string, table string) ([]Fiel
 		return nil, err
 	}
 
+	for key := range fields {
+		fields[key].SetStructVals()
+	}
+
 	return fields, nil
+}
+
+//
+func (f *Field) SetStructVals() {
+	f.SetFieldName()
+	f.SetDefault()
+	f.SetDataType()
+	f.SetAnnotation()
+}
+
+//
+func (f *Field) SetFieldName() {
+	f.FieldName = util.ToCamelCase(f.DbFieldName)
+}
+
+//
+func (f *Field) SetDefault() {
+
+	if !f.DbDefault.Valid {
+		return
+	}
+
+	if strings.Contains(f.DbDefault.String, "(") {
+		f.DbDefaultIsFunc = true
+	} else {
+		f.Default = f.DbDefault.String
+	}
+}
+
+//
+func (f *Field) SetDataType() {
+
+	switch f.DbDataType {
+	case "smallint":
+	case "smallserial":
+	case "serial":
+	case "bigint":
+	case "bigserial":
+	case "integer":
+		f.DataType = "sql.NullInt64"
+		return
+	case "boolean":
+		f.DataType = "sql.NullBool"
+		return
+	case "double precision":
+		f.DataType = "sql.NullFloat64"
+		return
+	}
+
+	f.DataType = "sql.NullString"
+}
+
+//
+func (f *Field) SetAnnotation() {
+	f.Annotation = fmt.Sprintf("`json:\"%s\" %s`", f.FieldName, f.GetValidation())
+}
+
+//
+func (f *Field) GetValidation() string {
+	val := "validate:"
+
+	// if not null and no default = required (Special case, bool = notnull)
+	// if not null with default = no insert/update (i.e., use default)
+	// if nullable = omitempty
+	if f.NotNull == true && !f.DbDefault.Valid {
+
+		if f.DbDataType == "boolean" {
+			val += `"notNull`
+		} else {
+			val += `"required`
+		}
+	} else {
+		val += `"omitempty`
+	}
+
+	switch f.DbDataType {
+	case "smallint", "smallserial", "serial", "integer", "bigint", "bigserial":
+		val += ",int"
+	case "double precision", "real":
+		val += ",numeric"
+	case "uuid":
+		val += ",uuid"
+	case "timestamp with time zone":
+		val += ",rfc3339"
+	case "timestamp without time zone":
+		val += ",rfc3339WithoutZone"
+	case "date":
+		val += ",date"
+	case "bool":
+		val += ",bool"
+	}
+
+	if strings.HasPrefix(f.DbDataType, "character varying(") {
+		re := regexp.MustCompile("[0-9]+")
+		val += ",min=1,max=" + re.FindString(f.DbDataType)
+	}
+
+	val += `"`
+
+	return val
 }
