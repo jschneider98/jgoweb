017cd70841ffb20bbeba2667375efdec57779531
James Schneider <jschneider98@gmail.com>
2019-10-16 20:47:01 -0700

    added BuildNameCondition()

--- params.go
+++ params.go
@@ -1,5 +1,11 @@
 package jgoweb
 
+import (
+	"fmt"
+	"strings"
+	"github.com/gocraft/dbr"
+)
+
 type SearchParams struct {
 	Query string
 	Limit uint64
@@ -8,4 +14,39 @@ type SearchParams struct {
 //
 func NewSearchParams() *SearchParams {
 	return &SearchParams{}
+}
+
+//
+func (sp *SearchParams) BuildNameCondition(alias string, firstName string, lastName string) (dbr.Builder, error) {
+	var builder dbr.Builder
+
+	if sp.Query == "" {
+		return nil, nil
+	}
+
+	queryParts := strings.Split(sp.Query, " ")
+
+	if len(queryParts) == 1 {
+		builder = dbr.And(
+			dbr.Or(
+				dbr.Expr( fmt.Sprintf("%s.%s ilike ?", alias, firstName), sp.Query + "%" ),
+				dbr.Expr( fmt.Sprintf("%s.%s ilike ?", alias, lastName), sp.Query + "%" ),
+			),
+		)
+
+		return builder, nil
+	}
+
+	builder = dbr.Or(
+		dbr.And(
+			dbr.Expr( fmt.Sprintf("%s.%s ilike ?", alias, firstName), queryParts[0] + "%" ),
+			dbr.Expr( fmt.Sprintf("%s.%s ilike ?", alias, lastName), queryParts[1] + "%" ),
+		),
+		dbr.And(
+			dbr.Expr( fmt.Sprintf("%s.%s ilike ?", alias, firstName), queryParts[1] + "%" ),
+			dbr.Expr( fmt.Sprintf("%s.%s ilike ?", alias, lastName), queryParts[0] + "%" ),
+		),
+	)
+
+	return builder, nil
 }
