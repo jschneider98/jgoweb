b9a4a1ad7a5f8a0bc07b67d792f67446d3433d1e
James Schneider <jschneider98@gmail.com>
2020-01-08 16:22:26 -0800

    improved Run() and ApplyUpdate(). Use context now instead of Tx.

--- system_db_update.go
+++ system_db_update.go
@@ -5,7 +5,6 @@ import(
 	"errors"
 	"database/sql"
 	"github.com/gocraft/web"
-	"github.com/gocraft/dbr"
 	"github.com/jschneider98/jgoweb/util"
 )
 
@@ -22,7 +21,7 @@ type SystemDbUpdateInterface interface {
 
 // SystemDbUpdate
 type SystemDbUpdate struct {
-	ApplyUpdate func(tx *dbr.Tx) error `json:"-" validate:"-"`
+	ApplyUpdate func(ctx ContextInterface) error `json:"-" validate:"-"`
 	Id sql.NullString `json:"Id" validate:"omitempty,int"`
 	UpdateName sql.NullString `json:"UpdateName" validate:"required,min=1,max=255"`
 	Description sql.NullString `json:"Description" validate:"required,min=1,max=255"`
@@ -419,19 +418,7 @@ func (sdu *SystemDbUpdate) NeedsToRun() (bool, error) {
 
 //
 func (sdu *SystemDbUpdate) Run() error {
-	tx, err := sdu.Ctx.OptionalBegin()
-
-	if err != nil {
-		return err
-	}
-
-	err = sdu.ApplyUpdate(tx)
-
-	if err != nil {
-		return err
-	}
-
-	return sdu.Ctx.OptionalCommit(tx)
+	return sdu.ApplyUpdate(sdu.Ctx)
 }
 
 //
