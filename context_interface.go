dc336c4134fc1a7bd1dd0d24021e5993733d6833
James Schneider <jschneider98@gmail.com>
2020-04-13 17:23:32 -0700

    added GetTemplate()

--- context_interface.go
+++ context_interface.go
@@ -1,16 +1,17 @@
 package jgoweb
 
-import(
+import (
 	"github.com/gocraft/dbr"
+	"github.com/gocraft/web"
 	jgoWebDb "github.com/jschneider98/jgoweb/db"
 	"gopkg.in/go-playground/validator.v9"
-	"github.com/gocraft/web"
+	"html/template"
 )
 
 type ContextInterface interface {
 	Begin() (*dbr.Tx, error)
-	Commit() (error)
-	Rollback() (error)
+	Commit() error
+	Rollback() error
 	Select(column ...string) *dbr.SelectBuilder
 	SelectBySql(query string, value ...interface{}) *dbr.SelectBuilder
 	InsertBySql(query string, value ...interface{}) *dbr.InsertStmt
@@ -24,4 +25,5 @@ type ContextInterface interface {
 	GetDbSession() *dbr.Session
 	SetDbSession(dbSess *dbr.Session)
 	GetValidator() *validator.Validate
+	GetTemplate(filename string) (*template.Template, error)
 }
