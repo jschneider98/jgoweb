bc124b69dbe50b36f7054fca823d0f4bb7fea09a
James Schneider <jschneider98@gmail.com>
2020-04-23 12:50:11 -0700

    temp

--- user.go
+++ user.go
@@ -549,7 +549,8 @@ func (u *User) SetPassword(password string, verifyPassword string) {
 	// If currentPassword is set, it must match the user's current password
 	currentPasswordVerified := u.currentPassword == "" || u.Authenticate(u.currentPassword)
 
-	fmt.Printf("\n\npasswordVerified: %v\n currentPasswordVerified: %v\n\n", passwordVerified, currentPasswordVerified)
+	fmt.Printf("\n***\ncurrent: %s\nraw: %s\nverify:%s\n", u.currentPassword, u.rawPassword, u.verifyRawPassword)
+	fmt.Printf("\n***\npasswordVerified: %v\n currentPasswordVerified: %v\n***\n", passwordVerified, currentPasswordVerified)
 
 	if !passwordVerified || !currentPasswordVerified {
 		u.Password.Valid = false
@@ -561,7 +562,7 @@ func (u *User) SetPassword(password string, verifyPassword string) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 	if err != nil {
-		fmt.Printf("\n\nERROR: %v\n\n", err)
+		fmt.Printf("\n***\nERROR: %v\n***\n", err)
 		u.Password.Valid = false
 		u.Password.String = ""
 	}
