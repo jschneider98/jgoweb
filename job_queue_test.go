125fc491bf63e9078c37fd96daf6edf80c0675c1
James Schneider <jschneider98@gmail.com>
2021-02-26 11:50:39 -0800

    more tests

--- job_queue_test.go
+++ job_queue_test.go
@@ -4,10 +4,11 @@ package jgoweb
 
 import (
 	"testing"
+	"time"
 )
 
 //
-func TestNewJobQueue(t *testing.T) {
+func TestJobQueueProcessJob(t *testing.T) {
 	InitMockCtx()
 	jqs, err := NewJobQueueNativeStore(MockCtx)
 
@@ -17,9 +18,50 @@ func TestNewJobQueue(t *testing.T) {
 
 	fac := &JobFactoryExample{}
 
-	_, err = NewJobQueue(jqs, fac)
+	jq, err := NewJobQueue(MockCtx, jqs, fac)
 
 	if err != nil {
 		t.Errorf("ERROR: %v", err)
+	}
+
+	sysJob, err := NewSystemJob(MockCtx)
+
+	if err != nil {
+		t.Errorf("ERROR: %v", err)
+	}
+
+	sysJob.SetName("test")
+	sysJob.SetDescription("test")
+
+	err = sysJob.Save()
+
+	if err != nil {
+		t.Errorf("ERROR: %v", err)
+	}
+
+	err = jq.Run()
+
+	if err != nil {
+		t.Errorf("ERROR: %v", err)
+	}
+
+	time.Sleep(200 * time.Millisecond)
+
+	sysJob, err = FetchSystemJobById(sysJob.Ctx, sysJob.GetId())
+
+	if err != nil {
+		t.Errorf("ERROR: %v\n", err)
+	}
+
+	if sysJob.GetError() != "" {
+		t.Errorf("ERROR: %v\n", sysJob.GetError())
+	}
+
+	if sysJob.GetStatus() == "" {
+		t.Errorf("ERROR: System Job status is blank, but should be set.\n")
+	}
+
+	if sysJob.GetEndedAt() == "" {
+		t.Errorf("ERROR: System Job endded at is blank, but should be set.\n")
 	}
 }
