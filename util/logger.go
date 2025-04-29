0258463ff668152bb9e3636ed968497912c1fbe5
James Schneider <jschneider98@gmail.com>
2025-04-29 15:23:30 -0700

    new logger

---
+++ util/logger.go
@@ -0,0 +1,39 @@
+package kdc
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"time"
+)
+
+var Logger *log.Logger
+
+// factory method
+func Log() *log.Logger {
+	if Logger != nil {
+		return Logger
+	}
+
+	Logger := log.New()
+
+	Logger.SetFormatter(&log.JSONFormatter{})
+
+	// @TEMP
+	Logger.SetLevel(log.DebugLevel)
+
+	return Logger
+}
+
+// defer Duration(Track("foo"))
+func Track(msg string) (string, time.Time) {
+	return msg, time.Now()
+}
+
+// debug duration to logger
+func Duration(msg string, start time.Time) {
+	Log().WithFields(
+		log.Fields{
+			"duration": fmt.Sprintf("%v", time.Since(start)),
+		},
+	).Debug(msg)
+}
